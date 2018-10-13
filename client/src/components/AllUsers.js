ac3b430f3f770749d2bc2c5ac4e502daa61ba453
Daniel Payne <42071158+ReadyProgrammer1@users.noreply.github.com>
2018-10-13 12:38:06 -0400

    Update AllUsers.js

--- client/src/components/AllUsers.js
+++ client/src/components/AllUsers.js
@@ -1,44 +1,28 @@
 import React from 'react'
-import { connect } from 'react-redux'
 
 import UserEntryTable from './table'
+
 import store from '../store'
 
 class UserEntries extends React.Component {
     constructor() {
         super();
-        this.state = {
-            email: '',
-            password: '',
-            errors: {},
-            mydata: {},
-        }
     }
     
   render() {
 
     const newUser = [] 
     newUser.push(store.getState().auth.user)
-    
-    return <div>
-      <h1 style={{color: '#8fcbff'}}>Account Info</h1>
-      <UserEntryTable newUser={newUser} loaded={''} />
-    </div>
-  }
-}
 
-function mapStateToProps(state) {
-  return {
-    //entries: state.userEntries.entries
-  }
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    persistUserEntries: (payload) => {
-      dispatch({ type: 'LOAD', payload })
+    if (Object.keys(store.getState().auth.user).length === 0) {
+        return <div></div>
+    } else {
+        return <div>
+            <h1 style={{color: '#8fcbff'}}>Account Info</h1>
+            <UserEntryTable newUser={newUser} loaded={''} />
+        </div>
     }
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(UserEntries)
+export default UserEntries
