8efd7fd18ff842a0ee5e7a1389d7ef0c4af8e9fb
Daniel Payne <42071158+ReadyProgrammer1@users.noreply.github.com>
2018-10-13 12:12:39 -0400

    Update AllUsers.js

--- client/src/components/AllUsers.js
+++ client/src/components/AllUsers.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import { getUser } from '../actions/authentication';
-
 import UserEntryTable from './table'
-
 import store from '../store'
 
 class UserEntries extends React.Component {
-  //this.props.loginUser(user);
     constructor() {
         super();
         this.state = {
@@ -17,73 +13,19 @@ class UserEntries extends React.Component {
             errors: {},
             mydata: {},
         }
-        //this.handleInputChange = this.handleInputChange.bind(this);
-        //this.handleSubmit = this.handleSubmit.bind(this);
-        this.showData = this.showData.bind(this)
-    }
-
-    showData(data) {
-        this.setState(state => {
-            state.myData = data;
-            return state;
-          });
-        //alert(this.state.myData)
-    }
-
-    componentWillMount() {
-        getNewUser()
-        function getNewUser() {
-            const myuser = {"email":"batman@us.gov","password":"password"}
-            getUser(myuser, (response) => {
-                console.log(response)
-                }
-            )
-        }
     }
     
-
   render() {
-    var entry = new Date();
-    
-    const allusers = {
-  
-        "users": [
-            {
-            "date": entry.toString(),
-            "name": "Batman",
-            'email': 'batman@us.gov',
-            'password': 'password'
-            },
-            {
-                "date": entry.toString(),
-                "name": 'Superman',
-                'email': 'superman@us.gov',
-                'password': 'password'
-            },
-            {
-                "date": entry.toString(),
-                "name": "Ironman",
-                'email': 'ironman@us.gov',
-                'password': 'password'
-            }   
-        ]
-    }
-
-    //alert(JSON.stringify(this.state.mydata))
 
     const newUser = [] 
     newUser.push(store.getState().auth.user)
-    //alert(newUser)
+    
     return <div>
       <h1 style={{color: '#8fcbff'}}>Account Info</h1>
       <UserEntryTable newUser={newUser} loaded={''} />
     </div>
   }
 }
-
-
-
-
 
 function mapStateToProps(state) {
   return {
